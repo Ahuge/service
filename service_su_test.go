4d61b595a49fcd966acc4d2e4d08f86180b116bc
Alex Hughes <ahughesalex@gmail.com>
2022-12-28 12:32:38 -0800

    Modify kardianos to Ahuge

--- service_su_test.go
+++ service_su_test.go
@@ -18,7 +18,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/kardianos/service"
+	"github.com/Ahuge/service"
 )
 
 const runAsServiceArg = "RunThisAsService"
--- service_test.go
+++ service_test.go
@@ -9,7 +9,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/kardianos/service"
+	"github.com/Ahuge/service"
 )
 
 func TestRunInterrupt(t *testing.T) {
